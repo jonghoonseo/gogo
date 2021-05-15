f0236333dc82538abf97133d0eaf04a57d61a745
Jonghoon Seo <jonghoon.seo@gmail.com>
2021-05-16 00:07:15 +0900

    Add Hangul to show go test

---
+++ hangul/hangul_test.go
@@ -0,0 +1,14 @@
+package hangul
+
+import "fmt"
+
+func ExampleHasConsonantSuffix() {
+	fmt.Println(HasConsonantSuffix("Go 언어"))
+	fmt.Println(HasConsonantSuffix("그럼"))
+	fmt.Println(HasConsonantSuffix("우리 밥 먹고 합시다."))
+
+	// Output:
+	// false
+	// true
+	// false
+}
